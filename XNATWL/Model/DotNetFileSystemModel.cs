67b85d89802e43a839d5dff6563fd0ddebc32786
chl0e3e <foxleychloe@gmail.com>
2022-12-29 11:52:24 +0000

    Property and PropertyList models, also fixed a compile error in DotNetFileSystemModel about the IO class not being imported correctly

--- XNATWL/Model/DotNetFileSystemModel.cs
+++ XNATWL/Model/DotNetFileSystemModel.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XNATWL.IO;
 using static System.Net.WebRequestMethods;
 
 namespace XNATWL.Model
---
+++ XNATWL/Model/Property.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XNATWL.Model
+{
+    public interface Property<T>
+    {
+        string Name
+        {
+            get;
+        }
+
+        bool IsReadOnly
+        {
+            get;
+        }
+
+        bool Nullable
+        {
+            get;
+        }
+
+        T Value
+        {
+            get;
+            set;
+        }
+        
+        Type Type
+        {
+            get;
+        }
+
+        event EventHandler<PropertyChangedEventArgs<T>> Changed;
+    }
+
+    public class PropertyChangedEventArgs<T> : EventArgs
+    {
+        public T Old;
+        public T New;
+        
+        public PropertyChangedEventArgs(T @old, T @new)
+        {
+            this.Old = @old;
+            this.New = @new;
+        }
+    }
+}
