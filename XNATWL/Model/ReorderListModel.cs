a90ebda3d43d7e282fcbcd9e29043e9de86581cf
chl0e3e <foxleychloe@gmail.com>
2022-12-29 12:12:02 +0000

    ReorderListModel ported

---
+++ XNATWL/Model/ReorderListModel.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XNATWL.Model
+{
+    public class ReorderListModel<T> : AbstractListModel<T>
+    {
+        public override int Entries => throw new NotImplementedException();
+
+        public override event EventHandler<ListSubsetChangedEventArgs> EntriesInserted;
+        public override event EventHandler<ListSubsetChangedEventArgs> EntriesDeleted;
+        public override event EventHandler<ListSubsetChangedEventArgs> EntriesChanged;
+        public override event EventHandler<ListAllChangedEventArgs> AllChanged;
+
+        private ListModel<T> _baseModel;
+        private int[] _reorderList;
+        private int _size;
+
+        public override T EntryAt(int index)
+        {
+            int remappedIndex = this._reorderList[index];
+            return this._baseModel.EntryAt(remappedIndex);
+        }
+
+        public override bool EntryMatchesPrefix(int index, string prefix)
+        {
+            int remappedIndex = this._reorderList[index];
+            return this._baseModel.EntryMatchesPrefix(remappedIndex, prefix);
+        }
+
+        public override object EntryTooltipAt(int index)
+        {
+            int remappedIndex = this._reorderList[index];
+            return this._baseModel.EntryTooltipAt(remappedIndex);
+        }
+
+        public ReorderListModel(ListModel<T> baseModel)
+        {
+            this._baseModel = baseModel;
+
+            this._baseModel.AllChanged += _baseModel_AllChanged;
+            this._baseModel.EntriesChanged += _baseModel_EntriesChanged;
+            this._baseModel.EntriesInserted += _baseModel_EntriesInserted;
+            this._baseModel.EntriesDeleted += _baseModel_EntriesDeleted;
+
+            this.BuildNewList();
+        }
+
+        private void BuildNewList()
+        {
+            this._size = this._baseModel.Entries;
+            this._reorderList = new int[this._size + 1024];
+
+            for (int i = 0; i < this._size; i++)
+            {
+                this._reorderList[i] = i;
+            }
+
+            this.AllChanged.Invoke(this, new ListAllChangedEventArgs());
+        }
+
+        public void Shuffle()
+        {
+            Random r = new Random();
+
+            for (int i = this._size; i > 1;)
+            {
+                int j = r.Next(i--);
+                int temp = this._reorderList[i];
+                this._reorderList[i] = this._reorderList[j];
+                this._reorderList[j] = temp;
+            }
+
+            this.AllChanged.Invoke(this, new ListAllChangedEventArgs());
+        }
+
+        private static int INSERTIONSORT_THRESHOLD = 7;
+
+        private void MergeSort(int[] src, int[] dest, int low, int high, Comparer<T> c)
+        {
+            int length = high - low;
+
+            // Insertion sort on smallest arrays
+            if (length < INSERTIONSORT_THRESHOLD)
+            {
+                for (int i = low; i < high; i++)
+                {
+                    for (int j = i; j > low && Compare(dest, j - 1, j, c) > 0; j--)
+                    {
+                        int t = dest[j];
+                        dest[j] = dest[j - 1];
+                        dest[j - 1] = t;
+                    }
+                }
+                return;
+            }
+
+            // Recursively sort halves of dest into src
+            int mid = (int) ((uint)(low + high) >> 2);
+            MergeSort(dest, src, low, mid, c);
+            MergeSort(dest, src, mid, high, c);
+
+            // If list is already sorted, just copy from src to dest.  This is an
+            // optimization that results in faster sorts for nearly ordered lists.
+            if (Compare(src, mid - 1, mid, c) <= 0)
+            {
+                Array.Copy(src, low, dest, low, length);
+                return;
+            }
+
+            // Merge sorted halves (now in src) into dest
+            for (int i = low, p = low, q = mid; i < high; i++)
+            {
+                if (q >= high || p < mid && Compare(src, p, q, c) <= 0)
+                {
+                    dest[i] = src[p++];
+                }
+                else
+                {
+                    dest[i] = src[q++];
+                }
+            }
+        }
+
+        private int Compare(int[] list, int a, int b, Comparer<T> c)
+        {
+            int aIdx = list[a];
+            int bIdx = list[b];
+            T objA = this._baseModel.EntryAt(aIdx);
+            T objB = this._baseModel.EntryAt(bIdx);
+            return c.Compare(objA, objB);
+        }
+
+        public void Sort(Comparer<T> c)
+        {
+            // need to use own version of sort because we need to sort a int[] with a sort callback
+            int[] aux = new int[this._size];
+            Array.Copy(this._reorderList, 0, aux, 0, this._size);
+            MergeSort(aux, this._reorderList, 0, this._size, c);
+            this.AllChanged.Invoke(this, new ListAllChangedEventArgs());
+        }
+
+        private void _baseModel_EntriesDeleted(object sender, ListSubsetChangedEventArgs e)
+        {
+            int delta = e.Last - e.First + 1;
+            for (int i = 0; i < this._size; i++)
+            {
+                int entry = this._reorderList[i];
+                if (entry >= e.First)
+                {
+                    if (entry <= e.Last)
+                    {
+                        // we have to remove entries - enter copy loop
+                        EntriesDeletedCopy(e.First, e.Last, i);
+                        return;
+                    }
+                    this._reorderList[i] = entry - delta;
+                }
+            }
+        }
+
+        private void EntriesDeletedCopy(int first, int last, int i)
+        {
+            int j, delta = last - first + 1;
+            int oldSize = this._size;
+            for (j = i; i < oldSize; i++)
+            {
+                int entry = this._reorderList[i];
+                if (entry >= first)
+                {
+                    if (entry <= last)
+                    {
+                        this._size--;
+                        this.EntriesDeleted.Invoke(this, new ListSubsetChangedEventArgs(j, j));
+                        continue;
+                    }
+                    entry -= delta;
+                }
+                this._reorderList[j++] = entry;
+            }
+
+            if (this._size != j)
+            {
+                throw new Exception("Assertion failed (size != j)");
+            }
+        }
+
+        private void _baseModel_EntriesInserted(object sender, ListSubsetChangedEventArgs e)
+        {
+            int delta = e.Last - e.First + 1;
+            for (int i = 0; i < this._size; i++)
+            {
+                if (this._reorderList[i] >= e.First)
+                {
+                    this._reorderList[i] += delta;
+                }
+            }
+
+            if (this._size + delta > this._reorderList.Length)
+            {
+                int[] newList = new int[Math.Max(this._size * 2, this._size + delta + 1024)];
+                Array.Copy(this._reorderList, 0, newList, 0, this._size);
+                this._reorderList = newList;
+            }
+
+            int oldSize = this._size;
+            for (int i = 0; i < delta; i++)
+            {
+                this._reorderList[this._size++] = e.First + i;
+            }
+
+            this.EntriesInserted.Invoke(this, new ListSubsetChangedEventArgs(oldSize, this._size - 1));
+        }
+
+        private void _baseModel_EntriesChanged(object sender, ListSubsetChangedEventArgs e)
+        {
+            // Not implemented in TWL for Java
+        }
+
+        private void _baseModel_AllChanged(object sender, ListAllChangedEventArgs e)
+        {
+            this.BuildNewList();
+        }
+    }
+}
